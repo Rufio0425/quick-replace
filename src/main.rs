f29214857fd4047e72b0f607fd2ecf12be804ebe
Sean Aguirre <seanpeter.aguirre@gmail.com>
2022-09-05 17:15:17 -0700

    read and write functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -10,8 +11,25 @@ struct Arguments {
 }
 
 fn main() {
-    print_usage();
-    parse_args();
+    let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to read from file '{}': {:?}",
+                    "Error:".red().bold().italic(), args.filename, e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("{} failed to wrtie to file '{}': {:?}",
+                    "Error:".red().bold().italic(), args.filename, e);
+            std::process::exit(1);
+        }
+    }
 }
 
 fn parse_args() -> Arguments {
